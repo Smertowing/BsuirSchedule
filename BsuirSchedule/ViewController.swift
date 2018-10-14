cb2343b79017a0ccb17669d74242cfb2ac3a4ae1
Kiryl <Kiryl.co@gmail.com>
2018-10-14 13:04:01 +0300

    Initial Commit

---
+++ BsuirSchedule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BsuirSchedule
+//
+//  Created by Kiryl Holubeu on 10/14/18.
+//  Copyright © 2018 Kiryl Holubeu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
